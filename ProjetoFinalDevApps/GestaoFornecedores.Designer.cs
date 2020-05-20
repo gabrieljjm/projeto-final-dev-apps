d11f0206bb333c96ca47713beec01b544d959af1
Gabriel Miranda <gaabriieelmiiraandaa@gmail.com>
2020-05-20 19:49:01 +0100

    Added "Criar" and "Apagar"

--- ProjetoFinalDevApps/GestaoFornecedores.Designer.cs
+++ ProjetoFinalDevApps/GestaoFornecedores.Designer.cs
@@ -28,12 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbCodPostal = new System.Windows.Forms.MaskedTextBox();
+            this.tbTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.tbNif = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbLocalidade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMorada = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.btApagar = new System.Windows.Forms.Button();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btCriar = new System.Windows.Forms.Button();
+            this.dgvFornecedores = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornecedores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbCodPostal
+            // 
+            this.tbCodPostal.Location = new System.Drawing.Point(96, 84);
+            this.tbCodPostal.Mask = "0000-000";
+            this.tbCodPostal.Name = "tbCodPostal";
+            this.tbCodPostal.Size = new System.Drawing.Size(52, 20);
+            this.tbCodPostal.TabIndex = 50;
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.Location = new System.Drawing.Point(96, 136);
+            this.tbTelefone.Mask = "000 000 000";
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(68, 20);
+            this.tbTelefone.TabIndex = 49;
+            // 
+            // tbNif
+            // 
+            this.tbNif.Location = new System.Drawing.Point(96, 111);
+            this.tbNif.Mask = "000 000 000";
+            this.tbNif.Name = "tbNif";
+            this.tbNif.Size = new System.Drawing.Size(68, 20);
+            this.tbNif.TabIndex = 48;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Telefone: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "NIF: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Código-Postal:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Localidade:";
+            // 
+            // tbLocalidade
+            // 
+            this.tbLocalidade.Location = new System.Drawing.Point(96, 58);
+            this.tbLocalidade.Name = "tbLocalidade";
+            this.tbLocalidade.Size = new System.Drawing.Size(266, 20);
+            this.tbLocalidade.TabIndex = 43;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Morada:";
+            // 
+            // tbMorada
+            // 
+            this.tbMorada.Location = new System.Drawing.Point(96, 32);
+            this.tbMorada.Name = "tbMorada";
+            this.tbMorada.Size = new System.Drawing.Size(266, 20);
+            this.tbMorada.TabIndex = 41;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Nome:";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(96, 6);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(266, 20);
+            this.tbNome.TabIndex = 39;
+            // 
+            // btApagar
+            // 
+            this.btApagar.Location = new System.Drawing.Point(281, 162);
+            this.btApagar.Name = "btApagar";
+            this.btApagar.Size = new System.Drawing.Size(75, 23);
+            this.btApagar.TabIndex = 38;
+            this.btApagar.Text = "Apagar";
+            this.btApagar.UseVisualStyleBackColor = true;
+            this.btApagar.Click += new System.EventHandler(this.btApagar_Click);
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.Location = new System.Drawing.Point(168, 162);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btAlterar.TabIndex = 37;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btCriar
+            // 
+            this.btCriar.Location = new System.Drawing.Point(57, 162);
+            this.btCriar.Name = "btCriar";
+            this.btCriar.Size = new System.Drawing.Size(75, 23);
+            this.btCriar.TabIndex = 36;
+            this.btCriar.Text = "Criar";
+            this.btCriar.UseVisualStyleBackColor = true;
+            this.btCriar.Click += new System.EventHandler(this.btCriar_Click);
+            // 
+            // dgvFornecedores
+            // 
+            this.dgvFornecedores.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dgvFornecedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFornecedores.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvFornecedores.Location = new System.Drawing.Point(12, 191);
+            this.dgvFornecedores.MultiSelect = false;
+            this.dgvFornecedores.Name = "dgvFornecedores";
+            this.dgvFornecedores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFornecedores.Size = new System.Drawing.Size(672, 247);
+            this.dgvFornecedores.TabIndex = 51;
+            // 
+            // GestaoFornecedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(696, 450);
+            this.Controls.Add(this.dgvFornecedores);
+            this.Controls.Add(this.tbCodPostal);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.tbNif);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbLocalidade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMorada);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.btApagar);
+            this.Controls.Add(this.btAlterar);
+            this.Controls.Add(this.btCriar);
+            this.Name = "GestaoFornecedores";
             this.Text = "GestaoFornecedores";
+            this.Load += new System.EventHandler(this.GestaoFornecedores_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornecedores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox tbCodPostal;
+        private System.Windows.Forms.MaskedTextBox tbTelefone;
+        private System.Windows.Forms.MaskedTextBox tbNif;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbLocalidade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMorada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Button btApagar;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btCriar;
+        private System.Windows.Forms.DataGridView dgvFornecedores;
     }
 }
--- ProjetoFinalDevApps/GestaoFornecedores.cs
+++ ProjetoFinalDevApps/GestaoFornecedores.cs
@@ -12,9 +12,136 @@ namespace ProjetoFinalDevApps
 {
     public partial class GestaoFornecedores : Form
     {
+        private RetrosariaModelContainer retrosaria;
+
         public GestaoFornecedores()
         {
             InitializeComponent();
+        }
+        private void GestaoFornecedores_Load(object sender, EventArgs e)
+        {
+            retrosaria = new RetrosariaModelContainer();
+            lerDadosFornecedores();
+        }
+        private void lerDadosFornecedores()
+        {
+            dgvFornecedores.DataSource = null;
+            dgvFornecedores.DataSource = retrosaria.FornecedorSet.ToList();
+        }
+        private void limpaCampos()
+        {
+            tbNome.Text = "";
+            tbLocalidade.Text = "";
+            tbMorada.Text = "";
+            tbCodPostal.Text = "";
+            tbNif.Text = "";
+            tbTelefone.Text = "";
+        }
+        private void btCriar_Click(object sender, EventArgs e)
+        {
+            bool preenchido = true;
+
+            //Obter informação digitada e atribuir esse valor às variàveis
+            Fornecedor novoFornecedor = new Fornecedor();
+            novoFornecedor.Nome = tbNome.Text;
+            novoFornecedor.Localidade = tbLocalidade.Text;
+            novoFornecedor.Morada = tbMorada.Text;
+            novoFornecedor.CodigoPostal = tbCodPostal.Text;
+            novoFornecedor.NIF = tbNif.Text;
+            novoFornecedor.Telefone = tbTelefone.Text;
+
+            //Verificar se todos os campos estão preenchidos
+            if (tbNome.Text == "")
+            {
+                tbNome.BackColor = Color.Aqua;
+                preenchido = false;
+            }
+            else
+            {
+                tbNome.BackColor = Color.White;
+            }
+            if (tbMorada.Text == "")
+            {
+                tbMorada.BackColor = Color.Aqua;
+                preenchido = false;
+            }
+            else
+            {
+                tbMorada.BackColor = Color.White;
+            }
+            if (tbLocalidade.Text == "")
+            {
+                tbLocalidade.BackColor = Color.Aqua;
+                preenchido = false;
+            }
+            else
+            {
+                tbLocalidade.BackColor = Color.White;
+            }
+            if (!tbCodPostal.MaskCompleted)
+            {
+                tbCodPostal.BackColor = Color.Aqua;
+                preenchido = false;
+            }
+            else
+            {
+                tbCodPostal.BackColor = Color.White;
+            }
+            if (!tbNif.MaskCompleted)
+            {
+                tbNif.BackColor = Color.Aqua;
+                preenchido = false;
+            }
+            else
+            {
+                tbNif.BackColor = Color.White;
+            }
+            if (!tbTelefone.MaskCompleted)
+            {
+                tbTelefone.BackColor = Color.Aqua;
+                preenchido = false;
+            }
+            else
+            {
+                tbTelefone.BackColor = Color.White;
+            }
+
+            if (preenchido)
+            {
+                //Adicionar cliente à base de dados
+                retrosaria.FornecedorSet.Add(novoFornecedor);
+                retrosaria.SaveChanges();
+
+                limpaCampos();
+
+                //Atualizar DataGridView
+                lerDadosFornecedores();
+            }
+        }
+
+        private void btApagar_Click(object sender, EventArgs e)
+        {
+            if (dgvFornecedores.SelectedRows == null)
+            {
+                MessageBox.Show("Selecione um Fornecedor");
+            }
+            else
+            {
+                string message = "Tem a certeza que deseja remover o fornecedor?";
+                string title = "Apagar cliente";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(message, title, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    Fornecedor selecionado = (Fornecedor)dgvFornecedores.CurrentRow.DataBoundItem;
+                    tbNome.Text = selecionado.Nome;
+                    retrosaria.FornecedorSet.Remove(selecionado);
+                    retrosaria.SaveChanges();
+
+                }
+
+                lerDadosFornecedores();
+            }
         }
     }
 }
