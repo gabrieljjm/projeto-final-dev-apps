a9b3a23744c76beda9c4a1570038c4856f3138ad
Francisco Jacinto <fjacinto77@gmail.com>
2020-05-24 14:45:12 +0100

    Changed menu design

--- ProjetoFinalDevApps/Menu.Designer.cs
+++ ProjetoFinalDevApps/Menu.Designer.cs
@@ -31,13 +31,19 @@
             this.btFormClientes = new System.Windows.Forms.Button();
             this.btFormPedidos = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btFormClientes
             // 
-            this.btFormClientes.Location = new System.Drawing.Point(12, 12);
+            this.btFormClientes.FlatAppearance.BorderSize = 0;
+            this.btFormClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btFormClientes.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btFormClientes.Location = new System.Drawing.Point(33, 125);
             this.btFormClientes.Name = "btFormClientes";
-            this.btFormClientes.Size = new System.Drawing.Size(75, 23);
+            this.btFormClientes.Size = new System.Drawing.Size(151, 59);
             this.btFormClientes.TabIndex = 0;
             this.btFormClientes.Text = "Clientes";
             this.btFormClientes.UseVisualStyleBackColor = true;
@@ -45,9 +51,12 @@
             // 
             // btFormPedidos
             // 
-            this.btFormPedidos.Location = new System.Drawing.Point(103, 12);
+            this.btFormPedidos.FlatAppearance.BorderSize = 0;
+            this.btFormPedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btFormPedidos.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btFormPedidos.Location = new System.Drawing.Point(33, 205);
             this.btFormPedidos.Name = "btFormPedidos";
-            this.btFormPedidos.Size = new System.Drawing.Size(75, 23);
+            this.btFormPedidos.Size = new System.Drawing.Size(151, 59);
             this.btFormPedidos.TabIndex = 1;
             this.btFormPedidos.Text = "Pedidos";
             this.btFormPedidos.UseVisualStyleBackColor = true;
@@ -55,24 +64,49 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(195, 12);
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(33, 281);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.Size = new System.Drawing.Size(151, 59);
             this.button1.TabIndex = 2;
             this.button1.Text = "Fornecedores";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.btFormClientes);
+            this.panel1.Controls.Add(this.btFormPedidos);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 637);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(903, 31);
+            this.panel2.TabIndex = 4;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btFormPedidos);
-            this.Controls.Add(this.btFormClientes);
+            this.ClientSize = new System.Drawing.Size(1103, 637);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Menu";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -82,6 +116,8 @@
         private System.Windows.Forms.Button btFormClientes;
         private System.Windows.Forms.Button btFormPedidos;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
