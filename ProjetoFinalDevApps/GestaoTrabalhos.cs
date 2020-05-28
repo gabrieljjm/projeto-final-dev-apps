902265bc7f2637c1912adf181abc02e3e0aec7b2
Francisco Jacinto <fjacinto77@gmail.com>
2020-05-28 19:20:05 +0100

    Changed "Exportar para PDF"

--- ProjetoFinalDevApps/GestaoTrabalhos.cs
+++ ProjetoFinalDevApps/GestaoTrabalhos.cs
@@ -26,48 +26,66 @@ namespace ProjetoFinalDevApps
         }
         public void ExportarPDF(DataGridView dgv, string filename)
         {
-            BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
-            PdfPTable pdftable = new PdfPTable(dgv.Columns.Count);
-            pdftable.DefaultCell.Padding = 3;
-            pdftable.WidthPercentage = 100;
-            pdftable.HorizontalAlignment = Element.ALIGN_LEFT;
-            pdftable.DefaultCell.BorderWidth = 1;
-
-            iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
-
-            foreach (DataGridViewColumn coluna in dgv.Columns)
+            try
             {
-                PdfPCell cell = new PdfPCell(new Phrase(coluna.HeaderText, text));
-                cell.BackgroundColor = new iTextSharp.text.BaseColor(216, 216, 191);
-                pdftable.AddCell(cell);
+                BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
+                PdfPTable pdftable = new PdfPTable(dgv.Columns.Count);
+                pdftable.DefaultCell.Padding = 3;
+                pdftable.WidthPercentage = 100;
+                pdftable.HorizontalAlignment = Element.ALIGN_LEFT;
+                pdftable.DefaultCell.BorderWidth = 1;
 
-            }
+                iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
 
-            foreach (DataGridViewRow linha in dgv.Rows)
-            {
-                foreach (DataGridViewCell cell in linha.Cells)
+                foreach (DataGridViewColumn coluna in dgv.Columns)
                 {
-                    pdftable.AddCell(new Phrase(cell.Value.ToString(), text));
-                }
-            }
+                    PdfPCell cell = new PdfPCell(new Phrase(coluna.HeaderText, text));
+                    cell.BackgroundColor = new iTextSharp.text.BaseColor(216, 216, 191);
+                    pdftable.AddCell(cell);
 
-            var saveFileDialog = new SaveFileDialog();
-            saveFileDialog.FileName = filename;
-            saveFileDialog.DefaultExt = ".pdf";
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
-                {
-                    Document pdfdoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
-                    PdfWriter.GetInstance(pdfdoc, stream);
-                    pdfdoc.Open();
-                    pdfdoc.Add(pdftable);
-                    pdfdoc.Close();
-                    stream.Close();
                 }
 
+                foreach (DataGridViewRow linha in dgv.Rows)
+                {
+                    foreach (DataGridViewCell cell in linha.Cells)
+                    {
+                        pdftable.AddCell(new Phrase(cell.Value.ToString(), text));
+                    }
+                }
+
+                var saveFileDialog = new SaveFileDialog();
+                saveFileDialog.FileName = filename;
+                saveFileDialog.DefaultExt = ".pdf";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    int soma = 0;
+                    using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                    {
+                        Document pdfdoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                        PdfWriter.GetInstance(pdfdoc, stream);
+                        pdfdoc.Open();
+                        pdfdoc.Add(pdftable);
+
+                        for (int i = 0; i < dgvTrabalhos.Rows.Count; ++i)
+                        {
+                            soma += Convert.ToInt32(dgvTrabalhos.Rows[i].Cells[6].Value);
+                        }
+                        Paragraph p = new Paragraph("Valor total: " + soma + " €");
+                        pdfdoc.Add(p);
+
+
+                        pdfdoc.Close();
+                        stream.Close();
+                    }
+
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
         private bool EstaSelecionado()
         {
